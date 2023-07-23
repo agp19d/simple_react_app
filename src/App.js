612cb11ba3831faa9c06ae00aa9ddd20359694ea
agp19d <agp19d@gmail.com>
2023-07-23 16:53:43 -0500

    refactored app to follow react create-react-app folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+import './styles.css';
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "learn react",
+      isCompleted: false,
+    },
+    {
+      text: "meet friend for lunch",
+      isCompleted: false,
+    },
+    {
+      text: "build todo app",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>To-Do List</h1>
+    <div className="app">
+      <div className="todo-list">
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo} />
+        ))}
+      </div>
     </div>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('ToDo', () => {
+  // Render the App component
+  const { getByText, getAllByText } = render(<App />);
+
+  // Test that 'To-Do List' heading is in the document
+  expect(getByText('To-Do List')).not.toBeNull();
+
+  // Test that there is at least one 'Remove' button in the document
+  expect(getAllByText('Remove').length).toBeGreaterThan(0);
 });
