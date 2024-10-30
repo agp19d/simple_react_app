8a9010c5c666cb951568926586d92d98e5725cdd
agp19d <agp19d@gmail.com>
2024-10-30 18:25:28 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Welcome to My Simple React App</h1>
+      <p>Deploying React on Azure App Services!</p>
+    </div>
+  );
+}
+
+export default App;
